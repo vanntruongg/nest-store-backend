a82e918bde095629893f8f8944af90faa690714e
Van Truong <truongtran.lv2019@gmail.com>
2024-03-19 21:32:16 +0700

    update

--- inventory-service/src/main/java/inventoryservice/constant/MessageConstant.java
+++ inventory-service/src/main/java/inventoryservice/constant/MessageConstant.java
@@ -1,4 +1,4 @@
-package nststore.userservice.constant;
+package inventoryservice.constant;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -9,18 +9,14 @@ public class MessageConstant {
   public static final String CHAR_SEQUENCE_3 = "%s %s %s";
   public static final String CHAR_SEQUENCE_4 = "%s %s %s %s";
   public static final String CHAR_SEQUENCE_5 = "%s %s %s %s %s";
-  public static final String SUCCESS = "successfully!";
+  public static final String SUCCESS = "thành công!";
 
 //  actions
-  public static final String FIND = "Find";
+  public static final String FIND = "Tìm";
 
 //  status
-  public static final String NOT_FOUND = "not found!";
+  public static final String NOT_FOUND = "không tìm thấy!";
 
-  public static final String USER = "User";
 
-  public static final String FIND_ALL_USER_SUCCESS = String.format(CHAR_SEQUENCE_2, FIND, SUCCESS);
 
-  public static final String USER_NOT_FOUND = String.format(CHAR_SEQUENCE_2, USER, NOT_FOUND);
-  public static final String EMAIL_EXISTED = "Email was existed!";
 }
--- product-service/src/main/java/vantruong/productservice/constant/MessageConstant.java
+++ product-service/src/main/java/vantruong/productservice/constant/MessageConstant.java
@@ -9,17 +9,17 @@ public class MessageConstant {
   public static final String CHAR_SEQUENCE_3 = "%s %s %s";
   public static final String CHAR_SEQUENCE_4 = "%s %s %s %s";
   public static final String CHAR_SEQUENCE_5 = "%s %s %s %s %s";
-  public static final String SUCCESS = "successfully!";
+  public static final String SUCCESS = "thành công!";
 
 //  actions
-  public static final String FIND = "Find";
-  public static final String CREATE = "Create";
+  public static final String FIND = "TÌm";
+  public static final String CREATE = "Tạo";
 
 //  status
-  public static final String NOT_FOUND = "not found!";
+  public static final String NOT_FOUND = "không tìm thấy!";
 
-  public static final String PRODUCT = "Product";
-  public static final String CATEGORY = "Category";
+  public static final String PRODUCT = "Sản phẩm";
+  public static final String CATEGORY = "Danh mục";
 
   public static final String FIND_SUCCESS = String.format(CHAR_SEQUENCE_2, FIND, SUCCESS);
 
