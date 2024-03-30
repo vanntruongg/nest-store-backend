e26c74360cd0cef32f9852bd7acbdbdb9fa4c7c7
Van Truong <truongtran.lv2019@gmail.com>
2024-03-30 23:53:27 +0700

    fix: add @Id redis hash

--- cart-service/src/main/java/vantruong/cartservice/entity/CartItem.java
+++ cart-service/src/main/java/vantruong/cartservice/entity/CartItem.java
@@ -1,6 +1,8 @@
-package vantruong.nststore.entity;
+package vantruong.cartservice.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.List;
 
@@ -9,7 +11,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@RedisHash("Cart")
 public class CartItem {
+  @Id
   private String email;
   private List<Item> items;
   private double totalPrice;
