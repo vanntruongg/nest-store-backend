15dec8435881726ac75ea1efe11b181fbbf07cce
Van Truong <truongtran.lv2019@gmail.com>
2024-03-30 23:51:34 +0700

    add handle exception

---
+++ cart-service/src/main/java/vantruong/cartservice/exception/ErrorDetail.java
@@ -0,0 +1,15 @@
+package vantruong.cartservice.exception;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetail {
+
+  private int errorCode;
+  private String message;
+
+}
---
+++ cart-service/src/main/java/vantruong/cartservice/exception/NotFoundException.java
@@ -0,0 +1,24 @@
+package vantruong.cartservice.exception;
+
+import lombok.Getter;
+
+@Getter
+public class NotFoundException extends RuntimeException {
+  private final transient ErrorDetail errorDetail;
+
+  public NotFoundException(int errorCode, String message) {
+    super(message);
+    this.errorDetail = ErrorDetail.builder()
+            .errorCode(errorCode)
+            .message(message)
+            .build();
+  }
+
+  public NotFoundException(int errorCode, String message, Throwable cause) {
+    super(message, cause);
+    this.errorDetail = ErrorDetail.builder()
+            .errorCode(errorCode)
+            .message(message)
+            .build();
+  }
+}
