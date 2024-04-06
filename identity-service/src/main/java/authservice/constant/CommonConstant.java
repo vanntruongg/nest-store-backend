02519bcdc78e9f5ede0e661b40cdd77284f2c213
Van Truong <truongtran.lv2019@gmail.com>
2024-04-06 23:56:58 +0700

    feat: add CommonConstant.java

---
+++ identity-service/src/main/java/authservice/constant/CommonConstant.java
@@ -0,0 +1,10 @@
+package authservice.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.NONE)
+public class CommonConstant {
+  public static final String EMAIL_URL = "http://localhost:9002";
+
+}
---
+++ order-service/src/main/java/orderservice/constant/CommonConstant.java
@@ -0,0 +1,11 @@
+package orderservice.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.NONE)
+public class CommonConstant {
+  public static final String PRODUCT_SERVICE_URL = "http://localhost:9002";
+  public static final String CART_SERVICE_URL = "http://localhost:9004";
+
+}
