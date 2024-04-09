6257aafc01012f2976e6e11e363dbb5bb35a2561
Van Truong <truongtran.lv2019@gmail.com>
2024-04-10 01:40:41 +0700

    update

--- order-service/src/main/java/orderservice/constant/ApiEndpoint.java
+++ order-service/src/main/java/orderservice/constant/ApiEndpoint.java
@@ -19,10 +19,15 @@ public class ApiEndpoint {
   public static final String ORDER = "/order";
   public static final String ORDERS = "/orders";
   public static final String ORDER_DETAIL = "/order-detail";
+  public static final String METHODS = "/methods";
 
   public static final String GET_URL_PAYMENT = "/get-link-payment";
-  public static final String CREATE_ORDER = CREATE + ORDER;
+  public static final String CREATE_ORDER = CREATE;
   public static final String GET_BY_EMAIL = GET + EMAIL;
   public static final String GET_BY_STATUS = GET + STATUS;
   public static final String GET_BY_EMAIL_AND_STATUS = GET + EMAIL + STATUS;
+  public static final String GET_BY_ID = GET + ID_PARAM;
+
+  //
+
 }
