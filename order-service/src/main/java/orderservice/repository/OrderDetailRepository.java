04043fc2558c1b262e1bd5c14037149089ec95b6
Van Truong <truongtran.lv2019@gmail.com>
2024-04-10 01:44:02 +0700

    update: add method findAllByOrder

--- order-service/src/main/java/orderservice/repository/OrderDetailRepository.java
+++ order-service/src/main/java/orderservice/repository/OrderDetailRepository.java
@@ -1,9 +1,13 @@
 package orderservice.repository;
 
+import orderservice.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import orderservice.entity.OrderDetail;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+  List<OrderDetail> findAllByOrder(Order order);
 }
