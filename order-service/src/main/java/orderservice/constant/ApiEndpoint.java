7d982674d6fd4df458ae1b4b2c388441733273e2
Van Truong <truongtran.lv2019@gmail.com>
2024-04-19 17:06:35 +0700

    update: add endpoint get revenue statistic

--- order-service/src/main/java/orderservice/constant/ApiEndpoint.java
+++ order-service/src/main/java/orderservice/constant/ApiEndpoint.java
@@ -20,16 +20,22 @@ public class ApiEndpoint {
   public static final String ORDERS = "/orders";
   public static final String ORDER_DETAIL = "/order-detail";
   public static final String METHODS = "/methods";
+  public static final String COUNT = "/count";
+  public static final String MONTH = "/month";
 
+  private static final String REVENUE = "/revenue";
   public static final String GET_URL_PAYMENT = "/get-link-payment";
-  public static final String CREATE_ORDER = CREATE;
   public static final String UPDATE = "/update";
+  public static final String GET_TOTAL_ORDER = "/total-order";
+  public static final String CREATE_ORDER = CREATE;
   public static final String GET_BY_EMAIL = GET + EMAIL;
   public static final String GET_BY_STATUS = GET + STATUS;
   public static final String GET_BY_EMAIL_AND_STATUS = GET + EMAIL + STATUS;
   public static final String GET_BY_ID = GET + ID_PARAM;
   public static final String UPDATE_STATUS = UPDATE + STATUS;
-  public static final String GET_TOTAL_ORDER = "/total-order";
+  public static final String COUNT_ORDER_BY_MONTH = COUNT + MONTH;
+  public static final String GET_ALL_REVENUE = GET + REVENUE;
+  public static final String GET_REVENUE_BY_MONTH = GET + REVENUE + MONTH;
 
   //
 
