0b7df8fdbbcc038bd8b763483f8b152f45f8610c
Van Truong <truongtran.lv2019@gmail.com>
2024-04-22 00:25:13 +0700

    update

--- product-service/src/main/java/vantruong/productservice/constant/ApiEndpoint.java
+++ product-service/src/main/java/vantruong/productservice/constant/ApiEndpoint.java
@@ -23,14 +23,18 @@ public class ApiEndpoint {
   public static final String CATEGORY = "/category";
   public static final String SUB_CATEGORY = "/subcategory";
   public static final String ALL_LEVEL = "/all-level";
+  public static final String TOP_LEVEL = "/top-level";
+  public static final String LIMIT = "/limit";
+  public static final String LIMIT_PARAM = "/{limit}";
 
   public static final String GET_ALL = "/get-all";
   public static final String PRODUCT_GET_BY_ID = GET + ID_PARAM;
-  public static final String PRODUCT_GET_BY_CATEGORY_ID = GET + CATEGORY + ID_PARAM;
+  public static final String PRODUCT_GET_BY_CATEGORY_ID = GET + CATEGORY + ID_PARAM + LIMIT + LIMIT_PARAM;
   public static final String CREATE_PRODUCT = CREATE;
   public static final String PRODUCT_GET_BY_NAME = GET + NAME;
   public static final String CATEGORY_GET_SUBCATEGORY = CATEGORY + SUB_CATEGORY + ID_PARAM;
   public static final String CATEGORY_GET_SUBCATEGORY_ALL_LEVEL = CATEGORY + SUB_CATEGORY + ALL_LEVEL + ID_PARAM;
   public static final String PRODUCT_GET_STOCK_BY_ID = GET + STOCK + ID_PARAM;
   public static final String UPDATE_PRODUCT = UPDATE;
+  public static final String TOP_LEVEL_CATEGORY = CATEGORY + TOP_LEVEL;
 }
