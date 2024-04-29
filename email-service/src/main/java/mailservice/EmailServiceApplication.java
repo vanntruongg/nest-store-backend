9a1e7bcdec07a6a2ba5b197c23da506a39bcb1d7
Van Truong <truongtran.lv2019@gmail.com>
2024-04-29 22:53:31 +0700

    update: EnableFeignClients

--- email-service/src/main/java/mailservice/EmailServiceApplication.java
+++ email-service/src/main/java/mailservice/EmailServiceApplication.java
@@ -2,10 +2,12 @@ package mailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableFeignClients
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
